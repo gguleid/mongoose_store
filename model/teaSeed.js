a870c9505bbbd19bd12159daa395ec75047c3612
Guled Ahmed <gguleid@gmail.com>
2021-06-08 17:26:32 -0400

    Initial commit

---
+++ model/teaSeed.js
@@ -0,0 +1,32 @@
+// Got the pictures and information from: https://imgur.com/gallery/Cr7r7
+module.exports = [{
+    name: 'Assam',
+    description: 'Caffeine: Medium',
+    img: "https://i.imgur.com/8i6Dur9.jpg",
+    price: 3,
+    qty: 150,
+},
+{
+    name: 'Berry Berry',
+    description: 'Caffeine: None',
+    img: "https://i.imgur.com/IbHyX7i.jpg",
+    price: 1,
+    qty: 300,
+},
+{
+    name: 'Gunpowder',
+    description: 'Caffeine: Low',
+    img: "https://i.imgur.com/amaJX7O.jpg",
+    price: 2,
+    qty: 200,
+},
+{
+    name: 'Jasmin Green',
+    description: 'Caffeine: Low',
+    img: "https://i.imgur.com/HCOG63B.jpg",
+    price: 5,
+    qty: 50,
+},
+
+];
+
---
+++ server.js
@@ -0,0 +1,114 @@
+// =================================================================================
+// Dependencies
+// =================================================================================
+const express = require('express');
+const mongoose = require('mongoose'); 
+const Tea = require('./model/products.js');
+const app = express();
+const port = 4005;
+
+
+// =================================================================================
+// Middleware
+// =================================================================================
+
+app.use(express.urlencoded({extended: false}));
+
+
+// =================================================================================
+// Configure Mongoose
+// =================================================================================
+
+const DATABASE_URL = 'mongodb+srv://admin:abc1234@cluster0.kpfq4.mongodb.net/teashop?retryWrites=true&w=majority';
+
+const db = mongoose.connection;
+
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+
+
+// =================================================================================
+// Mongodb Event Listeners
+// =================================================================================
+db.on('error', (error) => console.log(error.message + 'is mongodb not running?'));
+db.on('connected', () => console.log('mongodb connected'));
+db.on('disconnected', () => console.log('mongodb disconnected'));
+
+// =================================================================================
+// Set up Routes/Controllers - I.N.D.U.C.E.S
+// =================================================================================
+
+// Seed
+const teaSeed = require('./model/teaSeed.js');
+app.get('/shop/seed', (req, res) => {
+	Tea.deleteMany({}, (error, allTeas) => {})
+	Tea.create(teaSeed, (error, data) => {
+		res.redirect('/shop')
+	})
+})
+
+// Index
+app.get('/shop', (req, res) => {
+    Tea.find({}, (error, allTeas) => {
+        res.render('index.ejs', {
+            teas: allTeas,
+        });
+    });
+});
+
+
+// New
+app.get('/shop/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+
+// Delete
+
+
+
+// Update 
+
+
+
+// Create
+app.post('/shop', (req, res) => {
+    Tea.create(req.body, (error, newTea) => { //call back is a status update from mongoose
+        res.redirect('/shop');
+    });
+});
+
+// Edit
+
+
+// Show
+app.get('/shop/:id', (req, res) => {
+    Tea.findById(req.params.id, (error, foundTea) => {
+        res.render('show.ejs', {
+            tea: foundTea
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =================================================================================
+// Web express 
+// =================================================================================
+app.listen(port, () => console.log(`express is listening on port: ${port}`));
